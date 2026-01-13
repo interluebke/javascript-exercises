ffe67786a83271cf3b97b5d9a68cc2bf54148cfd
interluebke <111441242+interluebke@users.noreply.github.com>
2026-01-13 16:50:19 +0100

    Solve excercise 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(i, j) {
+    let arr = [];
+    if (i === j) {
+        return "ERROR";
+    } else if (!Number.isInteger(i) || !Number.isInteger(j)) {
+        return "ERROR";
+    } else if (i < 0 || j < 0) {
+        return "ERROR";
+    } else if (i > j) {
+        let k = i;
+        i = j;
+        j= k;
+    } 
+    for (let min = i; i <= j; i++) {
+        arr.push(i);
+    }
+    return arr.reduce((acc, curr) => acc + curr);
 };
 
 // Do not edit below this line
