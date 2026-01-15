dda785efa90e5c96a4333c850ca4590da93f1e42
interluebke <111441242+interluebke@users.noreply.github.com>
2026-01-15 16:31:18 +0100

    Solve excercise 12_calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) { 
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if (!arr || arr.length === 0) return 0;
+  if (arr.length === 1) return arr[0];
+  let sum = 0;
+  arr.forEach((num) => sum += num);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let sum = 1;
+  arr.forEach((num) => sum *= num);
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  let sum = 1;
+	for(let i = 0; i < num2; i++) sum *= num1;
+  return sum;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0 || num === 1) return 1;
+  let sum = 1;
+  for (let i = 1; i <= num; i++) sum *= i;
+  return sum;
 };
 
 // Do not edit below this line
