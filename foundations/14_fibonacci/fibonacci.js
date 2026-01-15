5b85364ecdd50f9d6c43de55db8637abbf2013ad
interluebke <111441242+interluebke@users.noreply.github.com>
2026-01-15 17:00:11 +0100

    Solve excercise 14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    number = Number(num);
+    if (number === 0) return 0;
+    if (number < 0) return "OOPS";
+    let fib = [1];
+    for (let i = 1; i <= number; i++) {
+        if (fib.length === 1) {
+            fib[i] = 1;
+        } else {
+            fib[i] = (fib[i - 1] + fib[i - 2]);
+        }
+    }
+    return fib[number - 1];
 };
 
 // Do not edit below this line
